57b83ee8d2cb8792c50720169b6b2b77a80f3813
DJ <dj@code-crew.org>
2025-04-17 12:50:29 -0500

    4n5 pass

--- submission/04.sh
+++ submission/04.sh
@@ -2,27 +2,27 @@
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 pubKeyHash=1e51fcdc14be9a148bb0aaec9197eb47c83776fb
-relative=1495584032
+time=1495584032
 
 
-if [ -z $relative ];
+if [ -z $time ];
 then
     echo "You must include an integer as an argument.";
     exit;
 fi
 
-if (( $relative > "2147483647" )) || (( $relative < "-2147483647" ));
+if (( $time > "2147483647" )) || (( $time < "-2147483647" ));
 then
-    echo "Your number ($relative) must be between -2147483647 and 2147483647";
+    echo "Your number ($time) must be between -2147483647 and 2147483647";
     exit;
 fi
 
-if [ $relative -lt 0 ];
+if [ $time -lt 0 ];
 then
-    integer=$(echo $((-$relative)));
+    integer=$(echo $((-$time)));
     negative=1;
 else
-    integer=$relative;
+    integer=$time;
     negative=0;
 fi
 
@@ -38,15 +38,21 @@ then
    lehex=$(printf '%x\n' $((0x$lehex | 0x80)))
 fi
 
-size=$(echo -n $lehex | wc -c | awk '{print $relative/2}');
+size=$(echo -n $lehex | wc -c | awk '{print $time/2}');
 hexcodeprefix=$(printf '%02x\n' $size);
 
-# relHash=0x$(. ../tools/int2le.sh $relative | cut -d ' ' -f2)
-# $relative OP_CHECKLOCKTIMEVERIFY
+# relHash=0x$(. ../tools/int2le.sh $time | cut -d ' ' -f2)
+# $time OP_CHECKLOCKTIMEVERIFY
 
-btcc $lehex OP_CHECKLOCKTIMEVERIFY OP_DROP OP_DUP OP_HASH160 $pubKeyHash OP_EQUALVERIFY OP_CHECKSIG
+# btcc $hexcodeprefix OP_CHECKLOCKTIMEVERIFY OP_DROP OP_DUP OP_HASH160 $pubKeyHash OP_EQUALVERIFY OP_CHECKSIG
+# echo 04 20cd2459 b1 75 76 a9 14 1e51fcdc14be9a148bb0aaec9197eb47c83776fb 88 ac
+# echo 0420cd2459b17576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac
+echo $hexcodeprefix$lehex"b17576a914"$pubKeyHash"88ac"
+
+
 # 0420cd2459b17576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac
 # 0420cd2459b17576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac
+#   20cd2459b17576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac
 # 0420cd2459b17576a92102e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb27788ac
 
 
--- submission/05.sh
+++ submission/05.sh
@@ -2,11 +2,48 @@
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 pubKeyHash=1e51fcdc14be9a148bb0aaec9197eb47c83776fb
+time=150
 
-# relHash=0x$(. ../tools/int2le.sh $relative | cut -d ' ' -f2)
-# $relative OP_CHECKLOCKTIMEVERIFY
+if [ -z $time ];
+then
+    echo "You must include an integer as an argument.";
+    exit;
+fi
 
-btcc 150 OP_CHECKSEQUENCEVERIFY OP_DROP OP_DUP OP_HASH160 $pubKeyHash OP_EQUALVERIFY OP_CHECKSIG
+if (( $time > "2147483647" )) || (( $time < "-2147483647" ));
+then
+    echo "Your number ($time) must be between -2147483647 and 2147483647";
+    exit;
+fi
+
+if [ $time -lt 0 ];
+then
+    integer=$(echo $((-$time)));
+    negative=1;
+else
+    integer=$time;
+    negative=0;
+fi
+
+hex=$(printf '%08x\n' $integer | sed 's/^\(00\)*//');
+
+hexfirst=$(echo $hex | cut -c1)
+[[ 0x$hexfirst -gt 0x7 ]] && hex="00"$hex
+
+lehex=$(echo $hex | tac -rs .. | echo "$(tr -d '\n')");
+
+if [ "$negative" -eq "1" ];
+then
+   lehex=$(printf '%x\n' $((0x$lehex | 0x80)))
+fi
+
+size=$(echo -n $lehex | wc -c | awk '{print $time/2}');
+hexcodeprefix=$(printf '%02x\n' $size);
+# relHash=0x$(. ../tools/int2le.sh $time | cut -d ' ' -f2)
+# $time OP_CHECKLOCKTIMEVERIFY
+
+# btcc 150 OP_CHECKSEQUENCEVERIFY OP_DROP OP_DUP OP_HASH160 $pubKeyHash OP_EQUALVERIFY OP_CHECKSIG
+echo $hexcodeprefix$lehex"b17576a914"$pubKeyHash"88ac"
 
 # 029600b27576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac
 # 
