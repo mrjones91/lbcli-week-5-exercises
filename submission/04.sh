523c075093577cc330f99b6041c347fbd208aa18
DJ <dj@code-crew.org>
2025-04-17 11:52:12 -0500

    4n5 pass

--- submission/04.sh
+++ submission/04.sh
@@ -1,28 +1,28 @@
 # Create a CLTV script with a timestamp of 1495584032 and public key below:
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
 publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
-pubKeyHash=0x"$publicKey"
+pubKeyHash=1e51fcdc14be9a148bb0aaec9197eb47c83776fb
 relative=1495584032
 
 
-if [ -z $1 ];
+if [ -z $relative ];
 then
     echo "You must include an integer as an argument.";
     exit;
 fi
 
-if (( $1 > "2147483647" )) || (( $1 < "-2147483647" ));
+if (( $relative > "2147483647" )) || (( $relative < "-2147483647" ));
 then
-    echo "Your number ($1) must be between -2147483647 and 2147483647";
+    echo "Your number ($relative) must be between -2147483647 and 2147483647";
     exit;
 fi
 
-if [ $1 -lt 0 ];
+if [ $relative -lt 0 ];
 then
-    integer=$(echo $((-$1)));
+    integer=$(echo $((-$relative)));
     negative=1;
 else
-    integer=$1;
+    integer=$relative;
     negative=0;
 fi
 
@@ -38,12 +38,16 @@ then
    lehex=$(printf '%x\n' $((0x$lehex | 0x80)))
 fi
 
-size=$(echo -n $lehex | wc -c | awk '{print $1/2}');
+size=$(echo -n $lehex | wc -c | awk '{print $relative/2}');
 hexcodeprefix=$(printf '%02x\n' $size);
 
 # relHash=0x$(. ../tools/int2le.sh $relative | cut -d ' ' -f2)
 # $relative OP_CHECKLOCKTIMEVERIFY
 
-$hexcodeprefix OP_CHECKLOCKTIMEVERIFY OP_DROP OP_DUP OP_HASH160 $pubKeyHash OP_EQUALVERIFY OP_CHECKSIG
+btcc $lehex OP_CHECKLOCKTIMEVERIFY OP_DROP OP_DUP OP_HASH160 $pubKeyHash OP_EQUALVERIFY OP_CHECKSIG
+# 0420cd2459b17576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac
 # 0420cd2459b17576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac
 # 0420cd2459b17576a92102e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb27788ac
+
+
+# 2102e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277a9a9 hash?
--- submission/05.sh
+++ submission/05.sh
@@ -1,2 +1,13 @@
 # Create a CSV script that would lock funds until one hundred and fifty blocks had passed
 # publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+publicKey=02e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb277
+pubKeyHash=1e51fcdc14be9a148bb0aaec9197eb47c83776fb
+
+# relHash=0x$(. ../tools/int2le.sh $relative | cut -d ' ' -f2)
+# $relative OP_CHECKLOCKTIMEVERIFY
+
+btcc 150 OP_CHECKSEQUENCEVERIFY OP_DROP OP_DUP OP_HASH160 $pubKeyHash OP_EQUALVERIFY OP_CHECKSIG
+
+# 029600b27576a9141e51fcdc14be9a148bb0aaec9197eb47c83776fb88ac
+# 
+# 029600b27576a92102e3af28965693b9ce1228f9d468149b831d6a0540b25e8a9900f71372c11fb27788ac
